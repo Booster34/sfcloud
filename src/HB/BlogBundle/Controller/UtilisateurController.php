dd5b294202ad5aeae5f854f0d5d2cb2db790287f
Booster34 <akabd@hotmail.fr>
2015-02-23 10:48:15 +0100

    bazar

--- src/HB/BlogBundle/Controller/UtilisateurController.php
+++ src/HB/BlogBundle/Controller/UtilisateurController.php
@@ -10,93 +10,115 @@ use HB\BlogBundle\Form\UtilisateurType;
 *
 * @author humanbooster
 *
-* @Route("/Utilisateur")
+* @Route("/utilisateur")
 */
 class UtilisateurController extends Controller
 {
 /**
-* Liste tous les Utilisateurs
+* Liste tous les utilisateurs
 *
-* @Route("/")
+* @Route("/", name="utilisateur_list")
 * @Template()
 */
 public function indexAction()
 {
 // on récupère le repository de l'Utilisateur
 $repository = $this->getDoctrine()->getRepository("HBBlogBundle:Utilisateur");
-// on demande au repository tous les Utilisateurs
-$Utilisateur = $repository->findAll();
+// on demande au repository tous les utilisateurs
+$utilisateurs = $repository->findAll();
 // on transmet la liste à la vue
-return array('Utilisateur' => $Utilisateur);
+return array('utilisateurs' => $utilisateurs);
 }
 /**
-* Affiche un formulaire pour ajouter un Utilisateur
+* Affiche un formulaire pour ajouter un utilisateur
 *
-* @Route("/add")
+* @Route("/add", name="utilisateur_add")
 * @Template()
 */
 public function addAction()
 {
-$Utilisateur = new Utilisateur();
+$utilisateur = new Utilisateur;
+return $this->editAction($utilisateur);
+}
+/**
+* Affiche un utilisateur sur un Id
+*
+* @Route("/{id}", name="utilisateur_read")
+* @Template()
+*/
+public function readAction(Utilisateur $utilisateur)
+{
+// on a récupéré l'utilisateur grace à un ParamConverter magique
+// on transmet notre utilisateur à la vue
+return array('utilisateur' => $utilisateur);
+}
+/**
+* Affiche un formulaire pour éditer un utilisateur sur un Id
+*
+* @Route("/{id}/edit", name="utilisateur_edit")
+* @Route("/titre/{titre}/edit")
+* @Template("HBBlogBundle:Utilisateur:add.html.twig")
+*/
+public function editAction(Utilisateur $utilisateur)
+{
 // on créé un objet formulaire en lui précisant quel Type utiliser
-$form = $this->createForm(new UtilisateurType, $Utilisateur);
+$form = $this->createForm(new UtilisateurType, $utilisateur);
 // On récupère la requête
 $request = $this->get('request');
 // On vérifie qu'elle est de type POST pour voir si un formulaire a été soumis
 if ($request->getMethod() == 'POST') {
 // On fait le lien Requête <-> Formulaire
-// À partir de maintenant, la variable $Utilisateur contient les valeurs entrées dans
+// À partir de maintenant, la variable $utilisateur contient les valeurs entrées dans
 // le formulaire par le visiteur
 $form->bind($request);
 // On vérifie que les valeurs entrées sont correctes
 // (Nous verrons la validation des objets en détail dans le prochain chapitre)
 if ($form->isValid()) {
-// On l'enregistre notre objet $Utilisateur dans la base de données
+// On l'enregistre notre objet $utilisateur dans la base de données
 $em = $this->getDoctrine()->getManager();
-$em->persist($Utilisateur);
+$em->persist($utilisateur);
 $em->flush();
-// On redirige vers la page de visualisation de l'Utilisateur nouvellement créé
+// On redirige vers la page de visualisation de l'utilisateur nouvellement créé
 return $this->redirect(
-$this->generateUrl('Utilisateur_read', array('id' => $Utilisateur->getId()))
+$this->generateUrl('utilisateur_read', array('id' => $utilisateur->getId()))
 );
 }
 }
+if ($utilisateur->getId()>0)
+$edition = true;
+else
+$edition = false;
 // passe la vue de formulaire à la vue
-return array( 'formulaire' => $form->createView() );
+return array( 'formulaire' => $form->createView(), 'edition' => $edition );
 }
 /**
-* Affiche un Utilisateur sur un Id
+* Supprime un utilisateur sur un Id
 *
-* @Route("/{id}", name="Utilisateur_read")
-* @Template()
+* @Route("/{id}/delete", name="utilisateur_delete")
 */
-public function readAction($id)
+public function deleteAction(Utilisateur $utilisateur)
 {
-// on récupère le repository de l'Utilisateur
-$repository = $this->getDoctrine()->getRepository("HBBlogBundle:Utilisateur");
-// on demande au repository l'Utilisateur par l'id
-$Utilisateur = $repository->find($id);
-// on transmet notre Utilisateur à la vue
-return array('Utilisateur' => $Utilisateur);
+// on a récupéré l'utilisateur grace à un ParamConverter magique
+// on demande à l'entity manager de supprimer l'utilisateur
+$em = $this->getDoctrine()->getEntityManager();
+$em->remove($utilisateur);
+$em->flush();
+// On redirige vers la page de liste des utilisateurs
+return $this->redirect(
+$this->generateUrl('utilisateur_list')
+);
 }
 /**
-* Affiche un formulaire pour éditer un Utilisateur sur un Id
+* Liste les articles d'un utilisateur
 *
-* @Route("/{id}/edit")
+* @Route("/{id}/articles", name="utilisateur_articles")
 * @Template()
-*/
-public function editAction($id)
-{
-return array();
-}
-/**
-* Supprime un Utilisateur sur un Id
 *
-* @Route("/{id}/delete")
-* @Template()
 */
-public function deleteAction($id)
+public function listArticlesAction(Utilisateur $utilisateur)
 {
-return array();
+return array('utilisateur' => $utilisateur,
+'articles' => $utilisateur->getArticles()
+);
 }
 }
